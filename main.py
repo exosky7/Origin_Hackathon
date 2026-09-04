36d880ce3cf5b4fcd9caa6b529069841454e8ea7
Aaditeya Verma <pyro.music.6@gmail.com>
2026-09-04 22:15:09 +0530

    random ahh update

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("HELL YEHAHHHH")
+import mysql.connector
+from mysql.connector import Error
+
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        database='ledger_data',
+        user='py_developer',
+        password='MySql1234!',
+    )
+
+    if connection.is_connected():
+        db_info = connection.get_server_info()
+        print(f"CONNECTED TO SQL VERSION {db_info}")
+
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute("select * from ledger_data")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+except Error as e:
+    print("FAILED TO CONNECT TO MYSQL")
+
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("DISCONNECTED FROM MYSQL")
+
